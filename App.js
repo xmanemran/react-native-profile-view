f3c2163dbd72265a4747e143785c6d9511c78c9e
Emran <xmanemran@gmail.com>
2018-04-17 12:07:03 +0600

    Add pen responder

--- App.js
+++ App.js
@@ -1,58 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+	Platform,
+	StyleSheet,
+	Text,
+	View,
+	PanResponder,
+	Animated
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+export default class App extends Component{
+	
+	componentWillMount() {
+		this._panResponder = PanResponder.create({
+			onMoveShouldSetResponderCapture: () => true,
+			onMoveShouldSetPanResponderCapture: () => true,
+			
+			onPanResponderGrant: (e, gestureState) => {
+				console.log(gestureState)
+			},
+			
+			onPanResponderMove: (e, gestureState) => {
+				console.log(gestureState)
+			},
+			
+			onPanResponderRelease: (e, {vx, vy}) => {
+			}
+		});
+	}
+	
+	
+	render() {
+		return (
+			<View style={styles.container} {...this._panResponder.panHandlers}>
+				<Text>OK</Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'red'
+	},
 });
